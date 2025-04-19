70aee8b659476f546cfae652c06249a85f0929ad
TuanVux <tuanvukg2703@gmail.com>
2025-04-19 14:56:58 +0700

    Register

---
+++ frontend/src/service/auth.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import authToken from '../storage/authToken.js';
+
+async function register(formData) {
+    try {
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/auth/register`, formData, {
+            headers: {
+                Authorization: `Bearer ${authToken.getToken()}`,
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error during registration:', error);
+        throw error;
+    }
+}
+
+async function login(formData) {
+    try {
+        const response = await axios.post('/api/auth/login', formData, {
+            headers: {
+                Authorization: `Bearer ${authToken.getToken()}`,
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error during login:', error);
+        throw error;
+    }
+}
+
+export { register, login }
