6da0ce0ab11d9eb54bc1a6104cb160653aab66e1
VyVictory <145247364+VyVictory@users.noreply.github.com>
2025-04-21 12:45:43 +0700

    update detail location

--- frontend/src/pages/map/Map.jsx
+++ frontend/src/pages/map/Map.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -6,10 +6,23 @@ import icon from "../../img/location.png";
 import { useLocation } from "../../context/LocationContext";
 import createCustomIcon from "./CreateCustomIcon";
 import LocationSetter from "./LocationSetter";
+import LocationModal from "../menu/LocationModal"; // import modal
+import { useMenu } from "../../context/MenuContext";
 
 export const Map = () => {
-  const { myLocation, setMyLocation, locationError, chaneLocation } =
-    useLocation();
+  const { toggleModel } = useMenu();
+  const {
+    myLocation,
+    setMyLocation,
+    locationError,
+    chaneLocation,
+    setLocationSelect,
+    locationSelect,
+  } = useLocation();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const selectedMarkerRef = useRef(null);
 
   const myLocationIcon = useMemo(() => createCustomIcon("Ô NÔ"), []);
   const selectedLocationIcon = useMemo(
@@ -18,16 +31,25 @@ export const Map = () => {
   );
 
   useEffect(() => {
-    // console.log(chaneLocation);
+    if (selectedMarkerRef.current) {
+      selectedMarkerRef.current.on("mouseover", function () {
+        selectedMarkerRef.current.openPopup();
+      });
+      selectedMarkerRef.current.on("mouseout", function () {
+        selectedMarkerRef.current.closePopup();
+      });
+    }
   }, [chaneLocation]);
+
   if (locationError) {
     return <div>{locationError}</div>;
   }
   if (!myLocation) {
     return <div>Loading your location...</div>;
   }
+
   return (
-    <div className="w-full h-screen  ">
+    <div className="w-full h-screen">
       <MapContainer
         className="h-full w-full max-w-screen max-h-[100dvh]"
         center={myLocation}
@@ -35,14 +57,25 @@ export const Map = () => {
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Marker position={myLocation} icon={myLocationIcon}>
-          <Popup>this, my point</Popup>
+          <Popup>This is my point</Popup>
         </Marker>
-        {/* Marker khi người dùng chọn */}
+
+        {/* Marker người dùng chọn */}
         {chaneLocation && (
-          <Marker position={chaneLocation} icon={selectedLocationIcon}>
+          <Marker
+            position={chaneLocation}
+            icon={selectedLocationIcon}
+            ref={selectedMarkerRef}
+            eventHandlers={{
+              click: () => (
+                toggleModel("location"), setLocationSelect(chaneLocation)
+              ),
+            }}
+          >
             <Popup>Bạn đã chọn vị trí này</Popup>
           </Marker>
         )}
+
         <LocationSetter />
       </MapContainer>
     </div>
